7783c3c337d1a4c2e4260683567fee28832f6c16
Jonás Perusquía Morales <jonaspm_99@outlook.com>
2017-01-27 14:14:51 -0700

    Update methods.js

--- imports/api/users/methods.js
+++ imports/api/users/methods.js
@@ -1,14 +1,17 @@
-import {Meteor} from 'meteor/meteor';
+import { Meteor } from 'meteor/meteor';
 
 Meteor.methods({
 	'users.search': function(params) {
-		const searchQuery = {$regex: '.*' + (params.filter || '') + '.*', $options: 'i'},
-			query = {},
-			options = {
-				limit: 10,
-				skip: (Number(params.page) - 1) * 10,
-				fields: {services: 0}
-			};
+		const query = {
+			'profile.name': {
+				$regex: '.*' + (params.filter || '') + '.*', $options: 'i'
+			}
+		},
+		options = {
+			limit: 10,
+			skip: (Number(params.page) - 1) * 10,
+			fields: {services: 0}
+		};
 
 		return {
 			total: Meteor.users.find(query).count(),
